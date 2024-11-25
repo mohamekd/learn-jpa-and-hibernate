a5aefc371a69b0d60303c66bcd8e947934e3601d
kood <mohamed.abdalla3398@gmail.com>
2024-11-25 12:25:35 +0200

    learn spring jpa and hibernate project - second commit make Insert query not Hard coded

--- src/main/java/com/kood/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/kood/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.kood.springboot.learn_jpa_hibernate.course.jdbc;
 
+import com.kood.springboot.learn_jpa_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
     CourseJdbcRepo jdbcRepo;
+
     @Override
     public void run(String... args) throws Exception {
-        jdbcRepo.insert();
+        jdbcRepo.insert(new Course(1, "Learn Spring", "Kood"));
     }
 //    @RequestMapping("/courses")
 //    public List<CourseJdbcRepo> retrieveAllCourses(){
--- src/main/java/com/kood/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcRepo.java
+++ src/main/java/com/kood/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcRepo.java
@@ -1,5 +1,6 @@
 package com.kood.springboot.learn_jpa_hibernate.course.jdbc;
 
+import com.kood.springboot.learn_jpa_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -9,12 +10,23 @@ public class CourseJdbcRepo {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    private  static String  INSERT_QUERY = """
-                insert into course(id,name,author)
-                values(1,'Learn Spring','in28Mins');
-                """;
-    public void insert(){
-        jdbcTemplate.update(INSERT_QUERY);
+    /*Hard Coded Query*/
+//    private  static String  INSERT_QUERY = """
+//                insert into course(id,name,author)
+//                values(1,'Learn Spring','Kood');
+//                """;
+//    public void insert(){
+//        jdbcTemplate.update(INSERT_QUERY);
+//    }
+
+    /*NOT Hard Coded Query*/
+    private static String INSERT_QUERY = """
+            insert into course(id,name,author)
+            values(?,?,?);
+            """;
+
+    public void insert(Course course) {
+        jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
     }
 
 }
